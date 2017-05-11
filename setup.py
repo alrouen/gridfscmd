b8697bea7d56e06bccdfc89043bf7cb1db2cc029
Alain Rouen <alain.rouen@kpmg.lu>
2017-05-11 09:53:31 +0200

    Setup fix

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='gridfscmd',
     version='0.1.0',
     packages=['gridfsOps'],
+    scripts = ['gridfscmd'],
     url='https://github.com/alrouen/gridfscmd',
     license='',
     author='alrouen',
-    author_email='',
+    author_email='alrouen@live.fr',
     description='Command line tool to interact with MongoDB GridFs',
-    install_requires = ["hurry.filesize==0.9", "pymongo=3.4.0"]
+    install_requires = [
+        "hurry.filesize==0.9",
+        "pymongo==3.4.0"
+    ]
 )
